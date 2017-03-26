ef5bc54c0adf7aedfd0a94ae145340e082623d06
Pristine Tran <pristinetran@tycoint.com>
2017-03-26 02:24:33 -0700

    Book Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+// to prevent 404
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { BookService } from './book.service';
+import { BookComponent } from './book/book.component';
+
+const ROUTES = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'books', component: BookComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [BookService],
+  providers: [
+    BookService,
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/book/book.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit {
+
+  books: any;
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    this.getBookList();
+  }
+
+  getBookList() {
+    this.bookService.getAllBooks().then((res) => {
+      this.books = res;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
