7f840781f75a2efdba5603d70baadcbb1bd62c8f
Pristine Tran <pristinetran@tycoint.com>
2017-03-26 02:53:16 -0700

    polish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 // to prevent 404
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
@@ -11,23 +11,27 @@ import { AppComponent } from './app.component';
 import { BookService } from './book.service';
 import { BookComponent } from './book/book.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
 
-const ROUTES = [
+const appRoutes: Routes = [
   { path: '', redirectTo: 'books', pathMatch: 'full' },
-  { path: 'books', component: BookComponent }
+  { path: 'books', component: BookComponent },
+  { path: 'book-details/:id', component: BookDetailComponent },
+  { path: 'book-create', component: BookCreateComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     BookComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    BookCreateComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     BookService,
